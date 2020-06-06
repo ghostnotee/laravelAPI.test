1c7d07dfc52825300001f77393938314d1482b15
Selçuk Bilgen <selcukbilgen@hotmail.com>
2020-06-06 15:40:24 +0300

    Logger Middleware added

---
+++ app/Http/Middleware/ApiLogger.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+
+class ApiLogger
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    // istek tamamlandıktan sonra çalışacak fonksiyon.
+    public function terminate(Request $request, Response $response)
+    {
+        $startTime = LARAVEL_START;
+        $endTime = microtime(true);
+        $log = '[' . date('Y-m-d H:i:s') . ']';
+        $log .= '[' . ($endTime - $startTime) * 100 . 'ms]';
+        $log .= '[' . $request->ip() . ']';
+        $log .= '[' . $request->method() . ']';
+        $log .= '[' . $request->fullUrl() . ']';
+
+        Log::info($log);
+    }
+}
